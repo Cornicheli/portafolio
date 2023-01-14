f4cf907ba4df40d8f332b76ca620259a871e6b08
cornicheli <gabrielcornide.pg@gmail.com>
2023-01-14 20:37:27 -0300

    fix button lightDark

---
+++ src/components/myproyect/MyProyect.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function MyProyect() {
+  return (
+    <div className="ctn-myproyect">
+      <div className="card-myproyect">
+        <div className="ctn-img-myproyect">
+          <img src="" alt="" />
+        </div>
+        <div className="ctn-text-myproyect">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, saepe
+            perspiciatis sed totam quis quod?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
