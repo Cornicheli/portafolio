7308cdc33912bfa726fff91d18a29e575752c3bd
cornicheli <gabrielcornide.pg@gmail.com>
2023-01-23 18:40:13 -0300

    fix ctn my skills

--- src/components/skill/Skills.jsx
+++ src/components/skill/Skills.jsx
@@ -16,7 +16,7 @@ export default function Skills() {
               alt=""
             />
           </div>
-          <h3 className="title-skill-logo">Front End</h3>
+          <h3 className="title-skill-logo-ft">Front End</h3>
           <div className="ctn-skill-card">
             <ul class="wrapper">
               <li class="icon html">
@@ -106,54 +106,56 @@ export default function Skills() {
             </ul>
           </div>
         </div>
-        <div className="ctn-skill-icon">
-          <div className="ctn-img-skill">
-            <img
-              className="icon-skill"
-              src="https://cdn-icons-png.flaticon.com/512/5380/5380760.png"
-              alt=""
-            />
-          </div>
-          <h3 className="title-skill-logo">Back End</h3>
-          <div className="ctn-skill-card">
-            <ul class="wrapper">
-              <li class="icon mongodb">
-                <span class="tooltip">Mongo DB</span>
-                <span>
-                  <i class="ctn-icon-skill-card">
-                    <img
-                      className="icon-skill-card"
-                      src="https://img.icons8.com/color/512/mongodb.png"
-                      alt=""
-                    />
-                  </i>
-                </span>
-              </li>
-              <li class="icon express">
-                <span class="tooltip">Express</span>
-                <span>
-                  <i class="ctn-icon-skill-card">
-                    <img
-                      className="icon-skill-card"
-                      src="https://img.icons8.com/ios/512/express-js.png"
-                      alt=""
-                    />
-                  </i>
-                </span>
-              </li>
-              <li class="icon nodejs">
-                <span class="tooltip">Node JS</span>
-                <span>
-                  <i class="ctn-icon-skill-card">
-                    <img
-                      className="icon-skill-card"
-                      src="https://img.icons8.com/color/512/nodejs.png"
-                      alt=""
-                    />
-                  </i>
-                </span>
-              </li>
-            </ul>
+        <div className="ctn-two-skill">
+          <div className="ctn-skill-icon">
+            <div className="ctn-img-skill">
+              <img
+                className="icon-skill"
+                src="https://cdn-icons-png.flaticon.com/512/5380/5380760.png"
+                alt=""
+              />
+            </div>
+            <h3 className="title-skill-logo">Back End</h3>
+            <div className="ctn-skill-card">
+              <ul class="wrapper">
+                <li class="icon mongodb">
+                  <span class="tooltip">Mongo DB</span>
+                  <span>
+                    <i class="ctn-icon-skill-card">
+                      <img
+                        className="icon-skill-card"
+                        src="https://img.icons8.com/color/512/mongodb.png"
+                        alt=""
+                      />
+                    </i>
+                  </span>
+                </li>
+                <li class="icon express">
+                  <span class="tooltip">Express</span>
+                  <span>
+                    <i class="ctn-icon-skill-card">
+                      <img
+                        className="icon-skill-card"
+                        src="https://img.icons8.com/ios/512/express-js.png"
+                        alt=""
+                      />
+                    </i>
+                  </span>
+                </li>
+                <li class="icon nodejs">
+                  <span class="tooltip">Node JS</span>
+                  <span>
+                    <i class="ctn-icon-skill-card">
+                      <img
+                        className="icon-skill-card"
+                        src="https://img.icons8.com/color/512/nodejs.png"
+                        alt=""
+                      />
+                    </i>
+                  </span>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="ctn-skill-icon">
             <div className="ctn-img-skill">
@@ -194,89 +196,6 @@ export default function Skills() {
             </div>
           </div>
         </div>
-
-        {/* <div className="ctn-skill-card">
-          <ul class="wrapper">
-            <li class="icon html">
-              <span class="tooltip">HTML</span>
-              <span>
-                <i class="fab fa-html-f">H</i>
-              </span>
-            </li>
-            <li class="icon css">
-              <span class="tooltip">CSS</span>
-              <span>
-                <i class="fab fa-css">C</i>
-              </span>
-            </li>
-            <li class="icon javascript">
-              <span class="tooltip">JavaScript</span>
-              <span>
-                <i class="fab fa-javascript">J</i>
-              </span>
-            </li>
-            <li class="icon bootstrap">
-              <span class="tooltip">Bootstrap</span>
-              <span>
-                <i class="fab fa-bootstrap">B</i>
-              </span>
-            </li>
-            <li class="icon mongodb">
-              <span class="tooltip">Mongo DB</span>
-              <span>
-                <i class="fab fa-mongodb">M</i>
-              </span>
-            </li>
-            <li class="icon express">
-              <span class="tooltip">Express</span>
-              <span>
-                <i class="fab fa-express">E</i>
-              </span>
-            </li>
-            <li class="icon react">
-              <span class="tooltip">React</span>
-              <span>
-                <i class="fab fa-react">R</i>
-              </span>
-            </li>
-            <li class="icon reactnative">
-              <span class="tooltip">React Native</span>
-              <span>
-                <i class="fab fa-reactnative">R</i>
-              </span>
-            </li>
-            <li class="icon nodejs">
-              <span class="tooltip">Node JS</span>
-              <span>
-                <i class="fab fa-nodejs">N</i>
-              </span>
-            </li>
-            <li class="icon redux">
-              <span class="tooltip">Redux Toolkit</span>
-              <span>
-                <i class="fab fa-redux">R</i>
-              </span>
-            </li>
-            <li class="icon typescript">
-              <span class="tooltip">TypeScript</span>
-              <span>
-                <i class="fab fa-typescript">T</i>
-              </span>
-            </li>
-            <li class="icon gitHub">
-              <span class="tooltip">Git Hub</span>
-              <span>
-                <i class="fab fa-gitHub">G</i>
-              </span>
-            </li>
-            <li class="icon postman">
-              <span class="tooltip">Postman</span>
-              <span>
-                <i class="fab fa-postman">P</i>
-              </span>
-            </li>
-          </ul>
-        </div> */}
       </div>
     </>
   );
