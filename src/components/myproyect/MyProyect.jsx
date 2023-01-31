506c482dd0e33de6fae87512ffbedabe95c504b3
cornicheli <gabrielcornide.pg@gmail.com>
2023-01-31 15:21:07 -0300

    finish my portfolio

--- src/components/myproyect/MyProyect.jsx
+++ src/components/myproyect/MyProyect.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Card from "../card/Card";
+import "./myProyect.css";
 
 export default function MyProyect() {
   return (
     <>
       <div className="ctn-img-myproyect" id="proyect">
-        <h1>hi</h1>
+        <h1 className="title-proyect">My Jobs</h1>
       </div>
-
       <Card />
     </>
   );
