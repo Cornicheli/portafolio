07ccbf5948d1c074302fa1f98387d5df69b102b1
cornicheli <gabrielcornide.pg@gmail.com>
2023-01-31 15:53:40 -0300

    fix

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -11,6 +11,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.discordapp.com/attachments/1063530094053818398/1068586251126571059/image.png"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -24,6 +25,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.discordapp.com/attachments/1063530094053818398/1068589271277117582/image.png"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -31,8 +33,8 @@ export default function Card() {
                 <h1>My Tinerary</h1>
 
                 <a
-                  target="_blank"
                   href="https://www.linkedin.com/in/gabriel-cornide-99624923b/"
+                  target="_blank"
                 >
                   view video
                 </a>
@@ -44,6 +46,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.discordapp.com/attachments/1063530094053818398/1068591212463931392/image.png"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -57,6 +60,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -74,6 +78,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -87,6 +92,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -100,6 +106,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg"
+                alt="proyect"
               />
             </div>
             <div class="details">
@@ -113,6 +120,7 @@ export default function Card() {
               <img
                 className="img-card"
                 src="https://cdn.pixabay.com/photo/2016/10/18/21/22/beach-1751455_1280.jpg"
+                alt="proyect"
               />
             </div>
             <div class="details">
